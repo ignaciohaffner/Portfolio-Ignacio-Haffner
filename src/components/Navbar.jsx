a9fd3fa6da8c14f64d107408187122ca9f186468
Ignacio Haffner <Ignaciohaffner@gmail.com>
2024-12-13 20:47:37 -0300

    Refactor image source and add smooth scroll functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,51 +1,33 @@
 const Navbar = () => {
+  const scrollToSection = (id) => {
+    const element = document.getElementById(id);
+    const offset = 100;
+    const bodyRect = document.body.getBoundingClientRect().top;
+    const elementRect = element.getBoundingClientRect().top;
+    const elementPosition = elementRect - bodyRect;
+    const offsetPosition = elementPosition - offset;
+
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div>
       <nav className="flex justify-center mx-auto">
-        <ul className="flex justify-center ">
+        <ul className="flex justify-center">
           <li className="p-5 hover:bg-white hover:text-black">
-            <a
-              onClick={() => {
-                document
-                  .getElementById("home")
-                  .scrollIntoView({ behavior: "smooth" });
-              }}
-            >
-              Home
-            </a>
+            <a onClick={() => scrollToSection("home")}>Home</a>
           </li>
           <li className="p-5 hover:bg-white hover:text-black">
-            <a
-              onClick={() => {
-                document
-                  .getElementById("aboutme")
-                  .scrollIntoView({ behavior: "smooth" });
-              }}
-            >
-              About me
-            </a>
+            <a onClick={() => scrollToSection("aboutme")}>About me</a>
           </li>
           <li className="p-5 hover:bg-white hover:text-black">
-            <a
-              onClick={() => {
-                document
-                  .getElementById("projects")
-                  .scrollIntoView({ behavior: "smooth" });
-              }}
-            >
-              Projects
-            </a>
+            <a onClick={() => scrollToSection("projects")}>Projects</a>
           </li>
           <li className="p-5 hover:bg-white hover:text-black">
-            <a
-              onClick={() => {
-                document
-                  .getElementById("contact")
-                  .scrollIntoView({ behavior: "smooth" });
-              }}
-            >
-              Contact
-            </a>
+            <a onClick={() => scrollToSection("contact")}>Contact</a>
           </li>
         </ul>
       </nav>
