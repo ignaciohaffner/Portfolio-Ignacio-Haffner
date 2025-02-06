8f7cce19f3de3b6550ac334f7e136829ef52d818
Ignacio Haffner <ignaciohaffner@gmail.com>
2025-02-06 00:02:19 -0300

    Update Certificates and Navbar components: modify certificate details and replace theme toggle icons with settings icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -75,14 +75,15 @@ const Navbar: React.FC = () => {
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <Button variant="outline" size="icon">
-                  <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                  <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                  <span className="sr-only">Toggle theme</span>
+                  <Settings className="h-[1.2rem] w-[1.2rem]" />
+                  <span className="sr-only">Settings</span>
                 </Button>
               </DropdownMenuTrigger>
               <DropdownMenuContent align="end">
                 <DropdownMenuLabel>{t.settings.appearance}</DropdownMenuLabel>
                 <DropdownMenuItem onClick={() => toggleTheme()}>
+                  <Moon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                  <Sun className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
                   {theme === "light"
                     ? t.settings.darkMode
                     : t.settings.lightMode}
@@ -124,6 +125,8 @@ const Navbar: React.FC = () => {
                       {t.settings.appearance}
                     </DropdownMenuLabel>
                     <DropdownMenuItem onClick={() => toggleTheme()}>
+                      <Moon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                      <Sun className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
                       {theme === "light"
                         ? t.settings.darkMode
                         : t.settings.lightMode}
